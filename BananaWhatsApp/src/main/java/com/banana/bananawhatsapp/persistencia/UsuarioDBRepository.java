3860d5d6d888cfb8954c1fe53ef13f592d16ec3b
cmorenodxc <cmoreno00@gmail.com>
2023-12-07 11:42:36 +0100

    us borrarUsuarios

--- BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/UsuarioDBRepository.java
+++ BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/UsuarioDBRepository.java
@@ -4,6 +4,8 @@ import com.banana.bananawhatsapp.exceptions.UsuarioException;
 import com.banana.bananawhatsapp.modelos.Usuario;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,12 +15,12 @@ import lombok.Setter;
 public class UsuarioDBRepository implements IUsuarioRepository{
 
 
-
     private String db_url;
 
     @Override
     public Usuario crear(Usuario usuario) throws SQLException {
          String sql = "INSERT INTO usuario values (NULL,?,?,?,?)";
+
 
         try (
                 Connection conn = DriverManager.getConnection(db_url);
@@ -37,12 +39,12 @@ public class UsuarioDBRepository implements IUsuarioRepository{
             if (genKeys.next()) {
                 usuario.setId(genKeys.getInt(1));
             } else {
-                throw new SQLException("Usuario creado erroneamente!!!");
+                throw new UsuarioException();
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new UsuarioException();
+            throw e;
         }
 
         return usuario;
@@ -51,16 +53,79 @@ public class UsuarioDBRepository implements IUsuarioRepository{
 
     @Override
     public Usuario actualizar(Usuario usuario) throws SQLException {
-        return null;
+        String sql = "UPDATE usuario set activo=?,alta=?,email=?,nombre=? where id=?";
+
+        try (
+                Connection conn = DriverManager.getConnection(db_url);
+                PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ) {
+
+            stmt.setInt(1, usuario.isActivo() ? 1 : 0);
+            stmt.setString(2, usuario.getAlta().toString());
+            stmt.setString(3, usuario.getEmail());
+            stmt.setString(4, usuario.getNombre());
+            stmt.setInt(5, usuario.getId());
+
+            int rows = stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return usuario;
     }
 
     @Override
     public boolean borrar(Usuario usuario) throws SQLException {
-        return false;
+        String sql = "DELETE FROM usuario WHERE id=?";
+
+        try (
+                Connection conn = DriverManager.getConnection(db_url);
+                PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ) {
+            stmt.setInt(1, usuario.getId());
+
+            int rows = stmt.executeUpdate();
+            System.out.println(rows);
+
+            if (rows <= 0) {
+                throw new UsuarioException();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return true;
     }
+
 
     @Override
     public Set<Usuario> obtenerPosiblesDestinatarios(Integer id, Integer max) throws SQLException {
-        return null;
+
+        Set<Usuario> listADevolver = new HashSet<>();
+
+         String sql = "SELECT * FROM usuario u WHERE 1";
+
+         try (
+                Connection conn = DriverManager.getConnection(db_url);
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery();
+        ) {
+
+            while (rs.next()) {
+                listADevolver.add(new Usuario(rs.getInt("id"), rs.getString("nombre"), rs.getString("email"),
+                         rs.getDate("alta").toLocalDate(),rs.getBoolean("activo")));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+
+        return listADevolver;
     }
 }
--- BananaWhatsApp/src/test/java/com/banana/bananawhatsapp/persistencia/UsuarioRepositoryTest.java
+++ BananaWhatsApp/src/test/java/com/banana/bananawhatsapp/persistencia/UsuarioRepositoryTest.java
@@ -10,9 +10,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.Matchers.is;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Set;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {SpringConfig.class})
@@ -28,8 +31,12 @@ class UsuarioRepositoryTest {
     }
     @Test
     void dadoUnUsuarioValido_cuandoCrear_entoncesUsuarioValido() throws SQLException {
-        Usuario us1 = new Usuario(null, "carmelo","cmoreno20",LocalDate.now(),true);
-        repo.crear(us1);
+        Usuario us1 = new Usuario(null, "jesus","cmoreno20@gmail.com",LocalDate.now(),true);
+
+        if(us1.valido()){
+           repo.crear(us1);
+        }
+
 
         System.out.println(us1);
 
@@ -37,27 +44,59 @@ class UsuarioRepositoryTest {
     }
 
     @Test
-    void dadoUnUsuarioNOValido_cuandoCrear_entoncesExcepcion() {
+    void dadoUnUsuarioNOValido_cuandoCrear_entoncesExcepcion(){
+        Usuario us1 = new Usuario(null, "jesus","cmoreno20@gmail.com",null,true);
+
+        assertThrows(Exception.class, () -> {
+            repo.crear(us1);
+        });
+
     }
 
     @Test
-    void dadoUnUsuarioValido_cuandoActualizar_entoncesUsuarioValido() {
+    void dadoUnUsuarioValido_cuandoActualizar_entoncesUsuarioValido() throws SQLException {
+        Usuario us1 = new Usuario(6, "CorPetit","carmelo@gmail.com",LocalDate.now(),true);
+        repo.actualizar(us1);
+        System.out.println(us1);
+
+        assertThat(us1.getId(), greaterThan(0));
+
     }
 
     @Test
     void dadoUnUsuarioNOValido_cuandoActualizar_entoncesExcepcion() {
+        Usuario us1 = new Usuario(4, "ander","cmoreno20@gmail.com",null,true);
+
+        assertThrows(Exception.class, () -> {
+            repo.actualizar(us1);
+        });
     }
 
     @Test
-    void dadoUnUsuarioValido_cuandoBorrar_entoncesOK() {
+    void dadoUnUsuarioValido_cuandoBorrar_entoncesOK() throws SQLException {
+        Usuario us1 = new Usuario(3, null, null, null, true);
+
+        boolean ok = repo.borrar(us1);
+
+        assertThat(ok, is(true));
     }
 
     @Test
     void dadoUnUsuarioNOValido_cuandoBorrar_entoncesExcepcion() {
+         Usuario us1 = new Usuario(3, null, null, null, true);
+
+        assertThrows(Exception.class, () -> {
+            repo.borrar(us1);
+        });
     }
 
     @Test
-    void dadoUnUsuarioValido_cuandoObtenerPosiblesDestinatarios_entoncesLista() {
+    void dadoUnUsuarioValido_cuandoObtenerPosiblesDestinatarios_entoncesLista() throws SQLException {
+        Set<Usuario> us = repo.obtenerPosiblesDestinatarios(1,1);
+
+        System.out.println(us);
+
+        assertThat(us.size(), greaterThan(0));
     }
 
     @Test
